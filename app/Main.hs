ef5c6e7352678bb5e76293ea8808ee9f0949ded6
Richard <captaincardo@gmail.com>
2021-12-01 19:34:26 -0800

    first commit

--- app/Main.hs
+++ app/Main.hs
@@ -1,5 +1,7 @@
 module Main where
 
+import History
+
 import Lib
 
 main :: IO ()
---
+++ src/History.hs
@@ -0,0 +1,11 @@
+module History (test) where
+
+-- history as trie
+-- https://hackage.haskell.org/package/bytestring-trie-0.2.6/docs/Data-Trie.html
+
+import Data.Trie as Trie
+
+findMatches :: Trie -> String -> [String]
+
+test :: ()
+test = ()
